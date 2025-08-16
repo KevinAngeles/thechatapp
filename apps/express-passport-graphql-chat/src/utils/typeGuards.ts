bd932a3449731fb6fe5e6d82e2639f804f76845e
KevinAngeles <kevin.angeles@outlook.com>
2025-08-15 19:43:18 -0500

    Create isAuthenticatedUser type guard to verify AuthenticatedUser interface. Use 'unknown' type for JWT payload to enforce type safetyand apply type guard to validate JWT payload in refresh-token endpoint

---
+++ apps/express-passport-graphql-chat/src/utils/typeGuards.ts
@@ -0,0 +1,8 @@
+import { AuthenticatedUser } from '../routes/auth.d';
+
+export function isAuthenticatedUser(user: unknown): user is AuthenticatedUser {
+    return user !== null && 
+           typeof user === 'object' && 
+           'id' in user && 
+           'nickname' in user;
+}
